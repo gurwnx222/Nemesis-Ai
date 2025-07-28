0d71e993d8433648d428d2c1723e39cccdf62777
gurwinx222 <threader.corp@gmail.com>
2025-07-28 08:29:52 +0530

    Refactor styles and layout: replace Geist font with Albert Sans, simplify global styles, and remove unused SVG assets.

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Albert_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const albertSans = Albert_Sans({
+  variable: "--font-albert-sans",
   subsets: ["latin"],
 });
 
@@ -19,11 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${albertSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
