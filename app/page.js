dbd1f76f97da6105001b5062425ec73b4f025bcd
gurwinx222 <threader.corp@gmail.com>
2025-07-28 08:46:21 +0530

    Added linting and prettier rules for better code formating, readablity, and enhanced code maintainance

--- app/page.js
+++ app/page.js
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+  return <h1 className="text-3xl font-bold">Hello World</h1>;
 }
