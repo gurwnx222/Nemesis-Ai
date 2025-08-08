7f80b4370fffd3c2d51e6c1fb32e28968688ae7e
YashChandan21 <yashparaschandan4@gmail.com>
2025-08-08 16:41:36 +0530

    Done with Feature Section

---
+++ components/ui/infinite-moving-cards.jsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import React, { useEffect, useState } from "react";
+
+export const InfiniteMovingCards = ({
+  items,
+  direction = "right",
+  speed = "fast",
+  pauseOnHover = true,
+  className
+}) => {
+  const containerRef = React.useRef(null);
+  const scrollerRef = React.useRef(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty("--animation-direction", "forwards");
+      } else {
+        containerRef.current.style.setProperty("--animation-direction", "reverse");
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "10s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        className
+      )}>
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          "flex w-[500px] min-w-full shrink-0 flex-nowrap gap-4 py-4  ",
+          start && "animate-scroll",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}>
+        {items.map((item, idx) => (
+          <li
+            className="relative w-[200px]  shrink-0 rounded-2xl   bg-[#1E1E1E]  px-8 py-6 md:w-[220px] dark:border-zinc-700 dark:bg-[linear-gradient(180deg,#27272a,#18181b)]"
+            key={item.name}>
+            <blockquote>
+              <div
+                aria-hidden="true"
+                className="user-select-none pointer-events-none absolute -top-0.5 -left-0.5 -z-1 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"></div>
+              <span
+                className="relative z-20 text-xl leading-[1.6] font-normal text-[#4F4F4F] dark:text-gray-100">
+                {item.title}
+              </span>
+              <div className="relative z-20 mt-6 flex flex-row items-center">
+                <span className="flex flex-col gap-1">
+                  <span
+                    className="text-sm leading-[1.6] font-normal text-[#656464] dark:text-gray-400">
+                    {item.quote}
+                  </span>
+                  <span
+                    className="text-sm leading-[1.6] font-normal text-[#4F4F4F] dark:text-gray-400">
+                    {/* {item.title} */}
+                  </span>
+                </span>
+              </div>
+            </blockquote>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- features/home/Feature.jsx
+++ features/home/Feature.jsx
@@ -1,24 +1,92 @@
+'use client';
 import React from 'react';
 import { AnimatedGradientTextDemo } from './components/AnimatedGradientTextDemo';
 import rocket from './components/rocket.png';
+import { AnimatedBeam } from '@/components/magicui/animated-beam';
+import { useRef } from 'react';
+import { DataFlowAnimation } from './components/DataFlowAnimation';
+import Image from 'next/image';
+import vector from './components/images/Vector.png';
+import vector2 from './components/images/Vector-2.png';
+import user_mutiple from './components/images/user-multiple-4.png';
+import click from './components/images/click.png';
+import { Ripple } from '@/components/magicui/ripple';
+import { AnimatedFollowers } from './components/AnimatedFollowers';
+import { InfiniteMovingCardsDemo } from './components/CardsAnimations';
 
 const Feature = () => {
+  const containerRef = useRef(null);
+  const fromRef = useRef(null);
+  const toRef = useRef(null);
+
   return (
-    <div className="text-white">
+    <div className="text-white mx-18 mt-30">
       {/* beam */}
       <AnimatedGradientTextDemo text={'Features'} img={rocket} />
 
       {/* Main Section */}
-      <main >
-        <div className="grid grid-cols-[2fr_1fr] border-2">
-          <div className="bg-gray-800 p-4 border-r-2">Left 1 </div>
-          <div className="bg-gray-700 p-4">Right 1 </div>
+      <main className='mt-20 '>
+        <div className="grid h-auto grid-cols-[2fr_1fr] border-2 border-[#9F9F9F]">
+          <div className=" p-4 border-r-2 border-[#9F9F9F]">
+            {/* Top Left section */}
+            <DataFlowAnimation />
+
+            <div className="flex flex-col gap-1.5 m-2 mt-15">
+              <Image src={vector} alt="vector" />
+              <h2 className="font-semibold text-2xl">
+                Automated Resource Sharing
+              </h2>
+              <p className=" text-[#757575]">
+                Bubble automatically responds to audience comments with relevant
+                resources so creators don't have to manually share them
+              </p>
+            </div>
+          </div>
+          <div className="bg-transparent  p-4">
+            <InfiniteMovingCardsDemo />
+            {/* Contnet */}
+            <div className="flex flex-col gap-1.5 m-2">
+              <Image src={click} alt='click' />
+              <h2 className="font-semibold text-2xl">Simple 3-Click Setup</h2>
+              <p className=" text-[#757575]">
+                Get started with the platform in a 3 click setup. Cutting down
+                hours of manual setup.
+              </p>
+            </div>
+          </div>
         </div>
 
         {/* <!-- Row 2: Wide Right --> */}
-        <div className="grid grid-cols-[1fr_2fr] border-x-2 border-b-2">
-          <div className="bg-gray-700 p-4 border-r-2">Left 2 </div>
-          <div className="bg-gray-800 p-4">Right 2 </div>
+        <div className="grid grid-cols-[1fr_2fr] border-x-2 border-b-2 border-[#9F9F9F]">
+          <div className="  border-r-2 border-[#9F9F9F]">
+            <div className="relative flex gap-1.5 h-[400px] w-full  flex-col items-center justify-center overflow-hidden rounded-lg   bg-transparent ">
+              <div className="z-10  text-white m-7 mt-50">
+                <Image src={vector2} alt="vector -2" />
+                <h2 className="font-semibold text-2xl">
+                  Creating Comment funnel
+                </h2>
+                <p className=" text-[#757575]">
+                  Bubble helps you create an exclusive comment funnels to boost
+                  your sales while you sleep.
+                </p>
+              </div>
+              <Ripple />
+            </div>
+          </div>
+          <div className=" p-4 ">
+            <AnimatedFollowers />
+
+            <div className="m-2">
+              <Image src={user_mutiple} alt="users" />
+              <h2 className="font-semibold text-2xl">
+                Automated DM Conversion
+              </h2>
+              <p className=" text-[#757575]">
+                Bubble sends conditional DMs to convert people into followers
+                (by the new Threads DM feature)
+              </p>
+            </div>
+          </div>
         </div>
       </main>
     </div>
